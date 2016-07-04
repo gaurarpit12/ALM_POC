060d03300896af4a79b732d4734e90cafe599530
gaurarpit12 <gaurarpit19@gmail.com>
2016-07-04 12:58:42 +0530

    xyz

--- result.bat
+++ result.bat
@@ -1,1 +1,1 @@
-"packages\NUnit.Runners.2.6.3\tools\nunit-console.exe" UnitTestProject.Test\bin\Debug\UnitTestProject.Test.dll
+"packages/NUnit.Runners.2.6.3/tools/nunit-console.exe" UnitTestProject.Test/bin/Debug/UnitTestProject.Test.dll
