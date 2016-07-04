f0ce4f4600a483bd2093e2c7827d0cdb483c0959
gaurarpit12 <gaurarpit19@gmail.com>
2016-07-04 16:54:58 +0530

    updated result.bat

--- result.bat
+++ result.bat
@@ -1,1 +1,1 @@
-"packages/NUnit.Runners.2.6.3/tools/nunit-console.exe" UnitTestProject.Test/bin/Debug/UnitTestProject.Test.dll
+"packages/NUnit.ConsoleRunner.3.4.0/tools/nunit3-console.exe" UnitTestProject.Test/bin/Debug/UnitTestProject.Test.dll
