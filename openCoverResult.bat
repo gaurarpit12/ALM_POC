db95ac5134558fa66bfff48620df92c907ea17d7
arpitga <arpitga@ARPITGA-W8>
2016-07-12 14:07:54 +0530

    added batch file for opencover

---
+++ openCoverResult.bat
@@ -0,0 +1,1 @@
+"%~dp0packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" "-register:path64" "-target:%~dp0packages\NUnit.ConsoleRunner.3.4.1\tools\nunit3-console.exe" "-targetargs:%~dp0UnitTestProject.Test\bin\Debug\UnitTestProject.Test.dll" "-filter:+[TestProject]*""
