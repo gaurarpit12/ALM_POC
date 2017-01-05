deb035ffb657fc2be925bda1c31e1fbd582fe560
gaurarpit12 <gaurarpit19@gmail.com>
2017-01-05 15:36:37 +0530

    Changed opencoverresult batch file

--- openCoverResult.bat
+++ openCoverResult.bat
@@ -1,1 +1,1 @@
-"%~dp0packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" "-register:path64" "-target:%~dp0packages\NUnit.ConsoleRunner.3.4.1\tools\nunit3-console.exe" "-targetargs:%~dp0UnitTestProject.Test\bin\Debug\UnitTestProject.Test.dll" "-filter:+[TestProject]*""
+"packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -target:"packages\NUnit.ConsoleRunner.3.4.1\tools\nunit3-console.exe" -targetargs:"UnitTestProject.Test\bin\Debug\UnitTestProject.Test.dll" -register:"user" "-filter:+[TestProject]*"
